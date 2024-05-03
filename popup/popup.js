be59c53886bd09064323fd7d1a6aff95a63ff7fc
Eder Joel <89995833+Ederjoel26@users.noreply.github.com>
2024-05-03 03:33:32 +0000

    Add files via upload
    New design and update because of WhatsApp Web updated design

--- popup/popup.js
+++ popup/popup.js
@@ -1,75 +1,167 @@
 const login = document.getElementById("login");
-const loged = document.getElementById("loged");
-const idGroupCookie = obtenerMensajeDeCookies();
+const loged = document.getElementById("logged");
+const inputUserName = document.getElementById("username");
+const inputPassword = document.getElementById("password");
+const btnReInicio = document.getElementById("reInicio");
+const btnLogin = document.getElementById("loginButton");
+let labelLine = document.getElementById("lineName");
+let labelContactsAdded = document.getElementById("contactsAdded");
+const linkExtension = "https://apiextension.gamblingproject.org/";
+let valorCookie = "";
 
-if(idGroupCookie === null){
-    login.style.display = "block";
-    loged.style.display = "none";
+const ejecutar = () => {
+    obtenerDatosDesdeStorage(async (datos) => {
+        try {
+            if (datos !== valorCookie) {
+                labelLine.innerText = await obtenerDatosUserLine();
+                login.style.display = "none";
+                loged.style.display = "block";
+                return;
+            }
+            login.style.display = "block";
+            loged.style.display = "none";
+            const response = await fetch(`${linkExtension}api/User/LoginExtension?UserName=${inputUserName.value}&Password=${inputPassword.value}`);
+            let data = "";
+            if(!response.ok){
+                data = await response.text();
+                if(data === "User no exists"){
+                    window.alert("El usuario ingresado no existe.");
+                }else if(data === "Incorrect password"){
+                    window.alert("La contraseña es incorrecta.");
+                }else if(data === "User is not active"){
+                    window.alert("El usuario está dado de baja.");
+                }else if(data === "Line not found"){
+                    window.alert("La linea a la que esta registrado no existe.");
+                }
+                return;
+            }
+            data = await response.json();
+            guardarDatosEnStorage(data.keyGroup);
+            guardarDatosUserLine(data.lineName);
+            guardarDatosOperator(inputUserName.value);
+            ocultarLogin();
+        } catch (error) {
+            console.error('Error en la ejecución:', error);
+        } finally {
 
-    const idGroup = generateRandomId();
-    
-    console.log(idGroup);
-    
-    generateQRCode(idGroup);
-    
-    const hubConnection = new signalR.HubConnectionBuilder()
-        .withUrl('https://savecontact.bsite.net/ExtensionHub')
-        .build();
-    
-    hubConnection.start().then(() => {
-        console.log('Conectado al hub de SignalR');
-        hubConnection.invoke('JoinGroup', idGroup)
-            .then(() => {
-                console.log(`Unido al grupo: ${idGroup}`);
-            })
-            .catch(error => {
-                console.error(`Error al unirse al grupo: ${error}`);
-            });
-    }).catch(err => {
-        console.error('Error al conectar al hub de SignalR:', err);
-    });
-    
-    hubConnection.on('ReceiveMessage', (user, message) => {
-        agregarMensajeACookies(message);
-        login.style.display = "none";
-        loged.style.display = "block";
-    });
-}else{
-    login.style.display = "none";
-    loged.style.display = "block";
-}
-
-
-function generateRandomId() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const timestamp = new Date().getTime();
-    const randomSuffix = Array.from({ length: 6 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
-    const randomId = `group_${timestamp}_${randomSuffix}`;
-    return randomId;
-}
-
-function agregarMensajeACookies(mensaje) {
-    const cookiesActuales = document.cookie;
-    const nuevaCookie = `idGroup=${encodeURIComponent(mensaje)}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-    document.cookie = cookiesActuales + ';' + nuevaCookie;
-}
-
-function obtenerMensajeDeCookies() {
-    const cookies = document.cookie;
-    const cookiesArray = cookies.split(';');
-    for (const cookie of cookiesArray) {
-        const [nombre, valor] = cookie.trim().split('=');
-        if (nombre === 'idGroup') {
-            return decodeURIComponent(valor);
         }
+    });
+} 
+
+ocultarLogin();
+
+btnLogin.addEventListener('click', () => {
+    ejecutar();
+});
+
+btnReInicio.addEventListener('click', () => {
+    obtenerDatosDesdeStorage( async datos => {
+        labelLine.innerText = await obtenerDatosUserLine();
+        labelContactsAdded.innerText = await contactosAgregadosOperador(await obtenerDatosOperator());
+        valorCookie = datos;
+        ocultarLogin();
+    });
+});
+
+async function contactosAgregadosOperador(nombreOperador) {
+    try {
+        console.log(nombreOperador);
+        const monthName = obtenerMesActual();
+        const response = await fetch(`${linkExtension}api/Contact/GetContactsOperatorPerMonth/MonthName?MonthName=${monthName}`);
+        const data = await response.json();
+        const operador = data.find(d => d.userName === nombreOperador);
+        if (!operador) {
+            return 0;
+        }
+        const contador = operador.contactsCount.reduce((total, element) => total + element.contacts, 0);
+        return contador;
+    } catch (error) {
+        console.error("Error en la función contactosAgregadosOperador:", error);
+        return 0;
     }
-    return null;
 }
 
-function generateQRCode(text) {
-  const qr = new QRious({
-    element: document.getElementById('qrcode'),
-    value: text,
-    size: 250, 
-  });
+// Función para obtener el nombre del mes actual
+function obtenerMesActual() {
+    const meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio",
+        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ];
+    const fechaActual = new Date();
+    const nombreMes = meses[fechaActual.getMonth()];
+    return nombreMes.charAt(0).toUpperCase() + nombreMes.slice(1);
+}
+
+
+function ObtenerMesActual(){
+    var fechaActual = new Date();
+    var meses = [
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ];
+    var nombreMes = meses[fechaActual.getMonth()];
+    nombreMes = nombreMes.charAt(0).toUpperCase() + nombreMes.slice(1);
+    return nombreMes;
+}
+
+function ocultarLogin() {
+    obtenerDatosDesdeStorage(async (datos) => {
+        if (datos !== valorCookie) {
+            login.style.display = "none";
+            loged.style.display = "block";
+            labelLine.innerText = await obtenerDatosUserLine();
+            labelContactsAdded.innerText = await contactosAgregadosOperador(await obtenerDatosOperator());
+            return;
+        }
+        login.style.display = "block";
+        loged.style.display = "none";
+    });
+}
+
+// Guardar datos en chrome.storage
+function guardarDatosEnStorage(datos) {
+    chrome.storage.local.set({ 'idGroup': datos }, function () {
+        console.log('Datos guardados en chrome.storage');
+    });
+}
+
+// Obtener datos desde chrome.storage
+function obtenerDatosDesdeStorage(callback) {
+    chrome.storage.local.get('idGroup', function (result) {
+        var datos = result.idGroup || '';
+        console.log('Datos obtenidos desde chrome.storage:', datos);
+        callback(datos);
+    });
+}
+
+function guardarDatosUserLine(datos) {
+    chrome.storage.local.set({ 'UserLine': datos }, function () {
+        console.log('Datos guardados en chrome.storage');
+    });
+}
+  
+async function obtenerDatosUserLine() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get('UserLine', function (result) {
+            var datos = result.UserLine || '';
+            console.log('Datos obtenidos desde chrome.storage UserLine:', datos);
+            resolve(datos);
+        });
+    });
+}
+
+function guardarDatosOperator(datos) {
+    chrome.storage.local.set({ 'Operator': datos }, function () {
+        console.log('Datos guardados en chrome.storage');
+    });
+}
+  
+async function obtenerDatosOperator() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get('Operator', function (result) {
+            var datos = result.Operator || '';
+            console.log('Datos obtenidos desde chrome.storage Operator:', datos);
+            resolve(datos);
+        });
+    });
 }
--- scripts/content.js
+++ scripts/content.js
@@ -1,91 +1,140 @@
 const Toast = Swal.mixin({
   toast: true,
-  position: "top-end",
+  position: "center",
   showConfirmButton: false,
-  timer: 3000,
+  timer: 1000,
   timerProgressBar: true,
   didOpen: (toast) => {
     toast.onmouseenter = Swal.stopTimer;
     toast.onmouseleave = Swal.resumeTimer;
-  }
+  },
 });
 
-document.addEventListener('click', function() {
-    const chat = document.querySelector('div[tabindex="0"]');
-    if (!chat) return;
-    const contactNotRegistered = document.querySelector('._3W2ap span.ggj6brxn');
-    if(!contactNotRegistered) return;
-    console.log(contactNotRegistered.innerHTML);
-    if(!isPhoneNumber(contactNotRegistered.innerHTML)) return;
-    const headerChat = document.querySelector("._1VwoK ._1sPvB");
-    const divExists = document.querySelector('div[id="AddContact"]');
-    if (divExists) return;
-    const divAddContact = document.createElement('div');
-    divAddContact.id = "AddContact";
-    divAddContact.style.height = "20px";
-    divAddContact.style.width = '20px';
-    const imageUrl = chrome.runtime.getURL('../images/contacto.png');
-    divAddContact.style.backgroundImage = `url('${imageUrl}')`;
-    divAddContact.style.backgroundSize = 'cover'; 
-    divAddContact.style.backgroundRepeat = 'no-repeat';
-    divAddContact.style.cursor = "pointer";
-    divAddContact.addEventListener('click', async () => {
-      await Swal.fire({
-        title: "Agregar contacto",
-        html: `<div style="display: flex; flex-direction: column; margin-bottom: 10px;">
-                <input type="text" id="swal-input1" placeholder="Ingresa el nombre" class="swal2-input">
-              </div>
-              `,
-        focusConfirm: false,
-        confirmButtonColor: "#0b9b84",
-        confirmButtonText: "Guardar contacto",
-        preConfirm: () => {
-          const name =  document.getElementById("swal-input1").value;
-          
-          if(name == "") return;
+document.addEventListener("click", function () {
+  const chat = document.querySelector('div[tabindex="0"]');
+  if (!chat) return;
+  const chatSelected = document.querySelector("._ak7p");
+  if (!chatSelected) return;
+  const contactNotRegistered = chatSelected.querySelector("._ak8q");
+  if (!contactNotRegistered) return;
+  let textoContacto = null;
+  try {
+    textoContacto = contactNotRegistered
+      .querySelector("._aou8 span")
+      .innerHTML.trim();
+  } catch (ex) {
+    return;
+  }
+  if (!isPhoneNumber(textoContacto)) return;
+  const headerContaier = document.querySelector("._ami8");
+  if (!headerContaier) return;
+  const headerChat = headerContaier.querySelector("._ajv2");
+  if (!headerChat) return;
+  const divExists = document.querySelector('div[id="AddContact"]');
+  if (divExists) return;
+  const divAddContact = document.createElement("div");
+  divAddContact.id = "AddContact";
+  divAddContact.style.height = "20px";
+  divAddContact.style.width = "20px";
+  const imageUrl = chrome.runtime.getURL("../images/contacto.png");
+  divAddContact.style.backgroundImage = `url('${imageUrl}')`;
+  divAddContact.style.backgroundSize = "cover";
+  divAddContact.style.backgroundRepeat = "no-repeat";
+  divAddContact.style.cursor = "pointer";
+  divAddContact.addEventListener("click", async () => {
+    await Swal.fire({
+      title: "Agregar contacto",
+      input: "text",
+      inputPlaceholder: "Ingresa el nombre",
+      focusConfirm: true,
+      confirmButtonColor: "#0b9b84",
+      showCancelButton: true,
+      confirmButtonText: "Guardar contacto",
+      preConfirm: async (name) => {
+        if (!name) return;
+        const datos = await obtenerDatosDesdeStorage();
+        console.log(datos);
+        const userLine = await obtenerDatosUserLine();
+        console.log(userLine);
+        const operator = await obtenerDatosOperator();
+        console.log(operator);
+        const contactData = {
+          _id: "",
+          Name: name,
+          PhoneNumber: textoContacto,
+          Date: new Date().toLocaleString("es-AR", {
+            timeZone: "America/Argentina/Buenos_Aires",
+          }),
+          IsRegistered: false,
+          IdGroup: datos,
+          OperatorUser: operator,
+          UserLine: userLine,
+        };
 
-          const contactData = {
-            Name: name,
-            Number: contactNotRegistered.innerHTML,
-            Date: new Date(),
-            isRegistered: false,
-            idGroup: obtenerMensajeDeCookies()
-          }
-          
-          const hubConnection = new signalR.HubConnectionBuilder()
-            .withUrl('https://savecontact.bsite.net/ExtensionHub')
-            .build();
-        
-            hubConnection.start().then(() => {
-                console.log('Conectado al hub de SignalR');
-                hubConnection.invoke('JoinGroup', "Eder Joel")
-                    .then(() => {
-                        console.log(`Unido al grupo: Eder Joel`);
-                        if(contactData.idGroup === null) {
-                          Toast.fire({
-                            icon: "error",
-                            title: "No estás logueado"
-                          });
-                          return;
-                        }
-                        hubConnection.invoke('SendMessage', "Eder Joel", JSON.stringify(contactData));
-                        Toast.fire({
-                          icon: "success",
-                          title: "Contacto agregado"
-                        });
-                        console.log('success');
-                    })
-                    .catch(error => {
-                        console.error(`Error al unirse al grupo: ${error}`);
+        console.log(contactData);
+        const hubConnection = new signalR.HubConnectionBuilder()
+          .withUrl("https://apiextension.gamblingproject.org/ExtensionHub")
+          .build();
+
+        hubConnection
+          .start()
+          .then(() => {
+            hubConnection
+              .invoke("JoinGroup", contactData.IdGroup)
+              .then(() => {
+                console.log(`Unido al grupo: ${contactData.IdGroup}`);
+                if (contactData.IdGroup === "") {
+                  Toast.fire({
+                    icon: "error",
+                    title: "No estás logueado",
+                  });
+                  return;
+                }
+                console.log(contactData);
+                hubConnection.invoke(
+                  "SendMessage",
+                  contactData.IdGroup,
+                  JSON.stringify(contactData)
+                );
+                const apiUrl =
+                  "https://apiextension.gamblingproject.org/api/Contact/CreateContact";
+                const requestOptions = {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(contactData),
+                };
+                fetch(apiUrl, requestOptions)
+                  .then((response) => {
+                    return response.json();
+                  })
+                  .then((data) => {
+                    console.log("Usuario creado exitosamente:", data);
+                    Toast.fire({
+                      icon: "success",
+                      title: "Contacto agregado",
                     });
-            }).catch(err => {
-                console.error('Error al conectar al hub de SignalR:', err);
-            });
-        }
-      });
-      
+                  })
+                  .catch((error) => {
+                    console.error("Error al crear el usuario:", error.message);
+                    Toast.fire({
+                      icon: "error",
+                      title: "Usuario creado anteriormente",
+                    });
+                  });
+              })
+              .catch((error) => {
+                console.error(`Error al unirse al grupo: ${error}`);
+              });
+          })
+          .catch((err) => {
+            console.error("Error al conectar al hub de SignalR:", err);
+          });
+      },
     });
-    headerChat.appendChild(divAddContact);
+  });
+  headerChat.appendChild(divAddContact);
 });
 
 function isPhoneNumber(str) {
@@ -93,14 +142,60 @@ function isPhoneNumber(str) {
   return regex.test(str);
 }
 
-function obtenerMensajeDeCookies() {
-  const cookies = document.cookie;
-  const cookiesArray = cookies.split(';');
-  for (const cookie of cookiesArray) {
-    const [nombre, valor] = cookie.trim().split('=');
-    if (nombre === 'idGroup') {
-      return decodeURIComponent(valor);
-    }
-  }
-  return null;
+function guardarDatosEnStorage(datos) {
+  chrome.storage.local.set({ idGroup: datos }, function () {
+    console.log("Datos guardados en chrome.storage");
+  });
+}
+
+function obtenerDatosDesdeStorage() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("idGroup", function (result) {
+      var datos = result.idGroup || "";
+      console.log("Datos obtenidos desde chrome.storage:", datos);
+      resolve(datos);
+    });
+  });
+}
+
+function guardarDatosUserLine(datos) {
+  chrome.storage.local.set({ UserLine: datos }, function () {
+    console.log("Datos guardados en chrome.storage");
+  });
+}
+
+async function obtenerDatosUserLine() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("UserLine", function (result) {
+      var datos = result.UserLine || "";
+      console.log("Datos obtenidos desde chrome.storage:", datos);
+      resolve(datos);
+    });
+  });
+}
+
+async function obtenerDatosDesdeStorageAsync() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("idGroup", function (result) {
+      var datos = result.idGroup || "";
+      console.log("Datos obtenidos desde chrome.storage:", datos);
+      resolve(datos);
+    });
+  });
+}
+
+function guardarDatosOperator(datos) {
+  chrome.storage.local.set({ 'Operator': datos }, function () {
+      console.log('Datos guardados en chrome.storage');
+  });
+}
+
+async function obtenerDatosOperator() {
+  return new Promise((resolve) => {
+      chrome.storage.local.get('Operator', function (result) {
+          var datos = result.Operator || '';
+          console.log('Datos obtenidos desde chrome.storage Operator:', datos);
+          resolve(datos);
+      });
+  });
 }
